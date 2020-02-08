68b0cbfd3f378c221516e8e9d671ca97076734c1
GraceeFlower <1491644103@qq.com>
2020-02-08 19:46:18 +0800

    [WM] feat: new the student Lily & print her info

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -3,6 +3,12 @@ import modul.Student;
 public class Application {
 
     public static void main(String[] args) {
+        Student liLy = new Student("李莉", 20, "女");
 
+        System.out.println(liLy.getName());
+        System.out.println(liLy.getAge());
+        System.out.println(liLy.getSex());
+        liLy.Study();
+        liLy.Rest();
     }
 }
