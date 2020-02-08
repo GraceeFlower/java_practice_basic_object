c171b4aa9fc93a4df700fce7642269748e0066f6
GraceeFlower <1491644103@qq.com>
2020-02-08 19:45:40 +0800

    [WM] feat: add the student class

---
+++ 002_object/src/main/java/modul/Student.java
@@ -0,0 +1,47 @@
+package modul;
+
+public class Student {
+
+    String name;
+    int age;
+    String sex;
+
+    public Student(String name, int age, String sex) {
+      this.name = name;
+      this.age = age;
+      this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void Study() {
+          System.out.println("I love studying!");
+      }
+
+      public void Rest() {
+          System.out.println("I always listen to music for rest.");
+      }
+
+}
